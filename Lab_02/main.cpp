88ba053048db9f2a7f245f755746cde588baf0ba
DudkoMatt <github.dudko.matt@mail.ru>
2020-03-11 14:01:06 +0300

    Lab_02: approved partial solution. Fixed some bugs: vertical drawing + initial brightness gamma correction

--- Lab_02/main.cpp
+++ Lab_02/main.cpp
@@ -9,11 +9,15 @@ void write_data(FILE *file_out, int k_bytes, unsigned char *pix_data);
 // void line_along_x(unsigned char *pix_data, int width, double x0, double y0, double x1, double y1);
 // void line_along_y(unsigned char *pix_data, int height, int width, double x0, double y0, double x1, double y1);
 
-void free_data(FILE *file_out, unsigned char *pix_data);
+void free_data(FILE *file_in, FILE *file_out, unsigned char *pix_data);
 
-void draw_pix(unsigned char *pix_data, int width, int x, int y, double brightness, double gamma);
+void draw_pix(unsigned char *pix_data, int width, int x, int y, double brightness, double gamma, int max_brightness);
 double fpart(double x);
-void wu_line_along_x(unsigned char *pix_data, int width, int height, double brightness, double x0, double y0, double x1, double y1, double gamma);
+//void wu_line_along_x(unsigned char *pix_data, int width, int height, double brightness, double x0, double y0, double x1, double y1, double gamma);
+
+
+// ToDO: along_y рисует вдоль обеих осей
+void wu_line_along_y(unsigned char *pix_data, int width, int height, int brightness, double x0, double y0, double x1, double y1, double gamma);
 
 
 /*
@@ -31,8 +35,8 @@ void wu_line_along_x(unsigned char *pix_data, int width, int height, double brig
 
 int main(int argc, char *argv[]) {
 
-    if (argc != 12 && argc != 11) {
-        std::cerr << "Wrong number of arguments. Syntax:\n<lab>.exe file_in file_out brightness thickness width height x0 y0 x1 y1 [gamma]";
+    if (argc != 9 && argc != 10) {
+        std::cerr << "Wrong number of arguments. Syntax:\n<lab>.exe file_in file_out brightness thickness x0 y0 x1 y1 [gamma]";
         return 1;
     }
 
@@ -41,7 +45,6 @@ int main(int argc, char *argv[]) {
 
     double brightness;
     double thickness;
-    int width, height;
     double x0, y0, x1, y1;
 
     // ToDO: sRGB
@@ -52,26 +55,46 @@ int main(int argc, char *argv[]) {
     // Лучше - 2.2
 
     try {
-        brightness = std::stoi(argv[3]) / 255.0;
+        brightness = std::stoi(argv[3]);
         thickness = std::stod(argv[4]);
-        width = std::stoi(argv[5]);
-        height = std::stoi(argv[6]);
-        x0 = std::stod(argv[7]);
-        y0 = std::stod(argv[8]);
-        x1 = std::stod(argv[9]);
-        y1 = std::stod(argv[10]);
-        if (argc == 12)
-            gamma = std::stod(argv[11]);
+        x0 = std::stod(argv[5]);
+        y0 = std::stod(argv[6]);
+        x1 = std::stod(argv[7]);
+        y1 = std::stod(argv[8]);
+        if (argc == 10)
+            gamma = std::stod(argv[9]);
     } catch (std::invalid_argument &e) {
         std::cerr << "Cannot convert one of the numbers from string to int\n";
         return 1;
     }
 
-    if (width <= 0 || height <= 0 || x0 < 0 || y0 < 0 || x1 < 0 || y1 < 0 || y0 >= height || y1 >= height ||
-        x0 >= width || x1 >= width || thickness <= 0 || brightness < 0 || brightness > 1) {
-        std::cerr << "Wrong arguments\n";
+
+    // Считывание размеров из файла
+
+    char _char_header;
+    int width, height;
+    unsigned int max_value;
+
+    FILE* file_in = fopen(file_in_name, "rb");
+
+    if (file_in == nullptr) {
+        std::cerr << "Cannot open file to read: " << file_in_name << "\n";
         return 1;
     }
+
+    int scanned = fscanf(file_in, "P%c\n%i %i\n%i\n", &_char_header, &width, &height, &max_value);
+
+
+    if (width <= 0 || height <= 0 || x0 < 0 || y0 < 0 || x1 < 0 || y1 < 0 || y0 >= height || y1 >= height ||
+        x0 >= width || x1 >= width || thickness <= 0 || brightness < 0 || brightness > 255) {
+        std::cerr << "Wrong arguments\n";
+        fclose(file_in);
+        return 1;
+    }
+
+
+    // Перевод из воспринимаемой яркости в фактическую
+    brightness = std::pow(brightness, gamma);
 
 /*
     // ToDO: преобразование не нужно
@@ -88,7 +111,7 @@ int main(int argc, char *argv[]) {
     auto pix_data = (unsigned char *) calloc(k_bytes, 1);
     if (!pix_data) {
         std::cerr << "Cannot allocate " << k_bytes << " bytes of memory\n";
-        free_data(file_out, pix_data);
+        free_data(file_in, file_out, pix_data);
         return 1;
     }
 
@@ -100,7 +123,7 @@ int main(int argc, char *argv[]) {
     file_out = fopen(file_out_name, "wb");
     if (file_out == nullptr) {
         std::cerr << "Cannot open file to write: " << file_out_name << "\n";
-        free_data(file_out, pix_data);
+        free_data(file_in, file_out, pix_data);
         return 1;
     }
 
@@ -136,14 +159,16 @@ int main(int argc, char *argv[]) {
     */
 
     // Алгоритм Ву со сглаживанием
-    wu_line_along_x(pix_data, width, height, brightness, x0, y0, x1, y1, gamma);
+    wu_line_along_y(pix_data, width, height, brightness, x0, y0, x1, y1, gamma);
 
     // ToDO: отдельно обработать случаи вертикальных и горизонтальных линий (в том числе и точки)
 
     write_header(file_out, char_header, width, height, 255);
     write_data(file_out, k_bytes, pix_data);
 
-    free_data(file_out, pix_data);
+    std::cout << (int) pix_data[0];
+
+    free_data(file_in, file_out, pix_data);
     return 0;
 }
 
@@ -156,7 +181,8 @@ void write_data(FILE *file_out, int k_bytes, unsigned char *pix_data) {
     fwrite(pix_data, k_bytes, 1, file_out);
 }
 
-void free_data(FILE *file_out, unsigned char *pix_data) {
+void free_data(FILE *file_in, FILE *file_out, unsigned char *pix_data) {
+    if (file_in) fclose(file_in);
     if (file_out) fclose(file_out);
     if (pix_data) free(pix_data);
 }
@@ -215,19 +241,19 @@ void line_along_y(unsigned char *pix_data, int height, int width, double x0, dou
 // С гамма коррекцией
 // Или с коррекцией sRGB, если гамма не передана
 // ToDO: проверять на границы выхода
-void draw_pix(unsigned char *pix_data, int width, int x, int y, double brightness, double gamma) {
+void draw_pix(unsigned char *pix_data, int width, int x, int y, double brightness, double gamma, int max_brightness) {
     // ToDO: коррекция
     brightness = std::min(std::max(brightness, 0.0), 1.0);
 
     // Гамма коррекция:
     if (gamma > 0) {
         // ToDO: обработка фона
-        brightness = std::pow(brightness, 1.0 / gamma);
+        brightness = std::pow(max_brightness * brightness, 1.0 / gamma);
     } else {
         // ToDO: sRGB
     }
 
-    pix_data[width * y + x] = std::floor(255 * brightness);
+    pix_data[width * y + x] = std::min(std::max((int) std::round(brightness), 0), 255);
 }
 
 double fpart(double x) {
@@ -235,7 +261,64 @@ double fpart(double x) {
 }
 
 // ToDO:
-void wu_line_along_x(unsigned char *pix_data, int width, int height, double brightness, double x0, double y0, double x1, double y1, double gamma) {
+// Previous version
+//void wu_line_along_x(unsigned char *pix_data, int width, int height, double brightness, double x0, double y0, double x1, double y1, double gamma) {
+//    // Swap if necessary
+//    if (x1 < x0) {
+//        double t = x0;
+//        x0 = x1;
+//        x1 = t;
+//
+//        t = y0;
+//        y0 = y1;
+//        y1 = t;
+//    }
+//
+//    double dx = x1 - x0;
+//    double dy = y1 - y0;
+//    double gradient = dy / dx;     // Угловой коэффициент прямой
+//
+//    // Обработка начальной точки
+//    // ToDO: у начальной точки могут быть не целые координаты => нужно сходу делать "дробление" по 2 пикселя
+//    double xend = round(x0);
+//    double yend = y0 + gradient * (xend - x0);
+//    double xgap = 1 - fpart(x0);
+//    double xpxl1 = xend;
+//    double ypxl1 = std::floor(yend);
+//
+//    draw_pix(pix_data, width, xpxl1, ypxl1, brightness * (1 - fpart(yend)) * xgap, gamma);
+//    // ToDO: обработать выход за границы
+//    draw_pix(pix_data, width, xpxl1, ypxl1 + 1, brightness * (fpart(yend)) * xgap, gamma);
+//
+//    double intery = yend + gradient;
+//
+//
+//    // Обработка конечной точки
+//    xend = round(x1);
+//    yend = y1 + gradient * (xend - x1);
+//    xgap = 1 - fpart(x1);
+//    double xpxl2 = xend;
+//    double ypxl2 = std::floor(yend);
+//
+//    draw_pix(pix_data, width, xpxl2, ypxl2, brightness * (1 - fpart(yend)) * xgap, gamma);
+//    // ToDO: обработать выход за границы
+//    draw_pix(pix_data, width, xpxl2, ypxl2 + 1, brightness * (fpart(yend)) * xgap, gamma);
+//
+//    for (int x = xpxl1 + 1; x < xpxl2; ++x) {
+//        draw_pix(pix_data, width, x, std::floor(intery), brightness * (1 - fpart(intery)), gamma);
+//        draw_pix(pix_data, width, x, std::floor(intery) + 1, brightness * fpart(intery), gamma);
+//        intery += gradient;
+//    }
+//
+//}
+/**/
+
+// ToDO:
+//  Note: "градиент" должен быть <= 1
+
+
+// ToDO:
+void wu_line_along_y(unsigned char *pix_data, int width, int height, int brightness, double x0, double y0, double x1, double y1, double gamma) {
     // Swap if necessary
     if (x1 < x0) {
         double t = x0;
@@ -249,7 +332,22 @@ void wu_line_along_x(unsigned char *pix_data, int width, int height, double brig
 
     double dx = x1 - x0;
     double dy = y1 - y0;
+
     double gradient = dy / dx;     // Угловой коэффициент прямой
+
+    if (dy > dx) {
+        gradient = dx / dy;
+
+        double t = x0;
+        x0 = y0;
+        y0 = t;
+
+        t = x1;
+        x1 = y1;
+        y1 = t;
+    }
+
+
 
     // Обработка начальной точки
     // ToDO: у начальной точки могут быть не целые координаты => нужно сходу делать "дробление" по 2 пикселя
@@ -259,9 +357,17 @@ void wu_line_along_x(unsigned char *pix_data, int width, int height, double brig
     double xpxl1 = xend;
     double ypxl1 = std::floor(yend);
 
-    draw_pix(pix_data, width, xpxl1, ypxl1, brightness * (1 - fpart(yend)) * xgap, gamma);
-    // ToDO: обработать выход за границы
-    draw_pix(pix_data, width, xpxl1, ypxl1 + 1, brightness * (fpart(yend)) * xgap, gamma);
+    // Отрисовка вдоль оси oY
+    if (dy > dx) {
+
+        draw_pix(pix_data, width, ypxl1, xpxl1, (1 - fpart(yend)) * xgap, gamma, brightness);
+        // ToDO: обработать выход за границы
+        draw_pix(pix_data, width, ypxl1 + 1, xpxl1, (fpart(yend)) * xgap, gamma, brightness);
+    } else {
+        draw_pix(pix_data, width, xpxl1, ypxl1, (1 - fpart(yend)) * xgap, gamma, brightness);
+        // ToDO: обработать выход за границы
+        draw_pix(pix_data, width, xpxl1, ypxl1 + 1, (fpart(yend)) * xgap, gamma, brightness);
+    }
 
     double intery = yend + gradient;
 
@@ -273,14 +379,55 @@ void wu_line_along_x(unsigned char *pix_data, int width, int height, double brig
     double xpxl2 = xend;
     double ypxl2 = std::floor(yend);
 
-    draw_pix(pix_data, width, xpxl2, ypxl2, brightness * (1 - fpart(yend)) * xgap, gamma);
-    // ToDO: обработать выход за границы
-    draw_pix(pix_data, width, xpxl2, ypxl2 + 1, brightness * (fpart(yend)) * xgap, gamma);
+    if (dy > dx) {
+        draw_pix(pix_data, width,  ypxl2, xpxl2, (1 - fpart(yend)) * xgap, gamma, brightness);
+        // ToDO: обработать выход за границы
+        draw_pix(pix_data, width,  ypxl2 + 1, xpxl2, (fpart(yend)) * xgap, gamma, brightness);
+    } else {
+
+        draw_pix(pix_data, width, xpxl2, ypxl2, (1 - fpart(yend)) * xgap, gamma, brightness);
+        // ToDO: обработать выход за границы
+        draw_pix(pix_data, width, xpxl2, ypxl2 + 1, (fpart(yend)) * xgap, gamma, brightness);
+    }
 
     for (int x = xpxl1 + 1; x < xpxl2; ++x) {
-        draw_pix(pix_data, width, x, std::floor(intery), brightness * (1 - fpart(intery)), gamma);
-        draw_pix(pix_data, width, x, std::floor(intery) + 1, brightness * fpart(intery), gamma);
+        if (dy > dx) {
+            draw_pix(pix_data, width,  std::floor(intery), x, (1 - fpart(intery)), gamma, brightness);
+            draw_pix(pix_data, width,  std::floor(intery) + 1, x,  fpart(intery), gamma, brightness);
+        } else {
+            draw_pix(pix_data, width, x, std::floor(intery),  (1 - fpart(intery)), gamma, brightness);
+            draw_pix(pix_data, width, x, std::floor(intery) + 1,  fpart(intery), gamma, brightness);
+        }
         intery += gradient;
     }
 
 }
+
+/*
+ * Лаба 3:
+ * диззеринг изображения
+ * --> пастеризация
+ *
+ *
+ *
+ *
+ *
+ * ToDO:
+ *  Баг:
+ *  file_in file_out_name.pgm 128 1 1920 1080 0 0 12 8 2.2
+ *  -
+ *  Цвет первого пикселя:
+ *  Должен быть 128
+ *  А не 180
+ *  -
+ *  -
+ *  -
+ *  Плюс баг с отрисовкой вдоль вертикальной оси
+ *
+ *
+ *
+ * ToDO:
+ *  Баг:
+ *  ./Lab_02.exe file_in file_out_name.pgm 128 1 200 200 0 0 10 15
+ *
+ */
