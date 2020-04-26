f3ca199df380bfdbb1ce3d535d6b8dc9fc291830
DudkoMatt <github.dudko.matt@mail.ru>
2020-04-26 21:08:13 +0300

    Lab_04: init commit

---
+++ Lab_04/write_data_pnm.cpp
@@ -0,0 +1,121 @@
+//
+// Created by dudko on 11.03.2020.
+//
+
+#include "write_data_pnm.h"
+#include <cmath>
+
+void write_header(FILE *file_out, char char_header, int width, int height, unsigned int max_value) {
+    fseek(file_out, 0, SEEK_SET);
+    fprintf(file_out, "P%c\n%i %i\n%i\n", char_header, width, height, max_value);
+}
+
+void write_data(FILE *file_out, int k_bytes, unsigned char *pix_data) {
+    fwrite(pix_data, k_bytes, 1, file_out);
+}
+
+void free_data(FILE *file_in, FILE *file_out, unsigned char *pix_data) {
+    if (file_in) fclose(file_in);
+    if (file_out) fclose(file_out);
+    if (pix_data) free(pix_data);
+}
+
+void free_data(FILE *file, unsigned char *pix_data) {
+    free_data(nullptr, file, pix_data);
+}
+
+void free_data(FILE *file) {
+    free_data(file, nullptr);
+}
+
+void free_data(unsigned char *pix_data) {
+    free_data(nullptr, pix_data);
+}
+
+void write_to_file(FILE *file_out, char char_header, int width, int height, unsigned int max_value,
+                   unsigned char *pix_data) {
+    write_header(file_out, char_header, width, height, max_value);
+    write_data(file_out, width * height, pix_data);
+}
+
+// brightness in [0..255] scale
+unsigned char limit_brightness(double brightness) {
+    return (unsigned char) std::min(255.0, std::max(0.0, brightness));
+}
+
+double from_sRGB(double _brightness) {
+    if (_brightness <= 0.0031308) {
+        return 323.0 * _brightness / 25.0;
+    } else {
+        return (211 * pow(_brightness, 5.0 / 12.0) - 11) / 200.0;
+    }
+}
+
+double from_sRGB(int brightness) {
+    double _brightness = brightness / 255.0;
+    return from_sRGB(_brightness);
+}
+
+double to_sRGB(double _brightness) {
+    if (_brightness <= 0.04045) {
+        return 25.0 * _brightness / 323;
+    } else {
+        return pow((200 * _brightness + 11) / 211.0, 12.0 / 5.0);
+    }
+}
+
+double to_sRGB(int brightness) {
+    double _brightness = brightness / 255.0;
+    return to_sRGB(_brightness);
+}
+
+double change_pix_gamma_to_print(double _brightness, double gamma) {
+    // Гамма коррекция:
+    if (gamma > 0) {
+        _brightness = std::pow(_brightness, 1.0 / gamma);
+    } else {
+        _brightness = from_sRGB(_brightness);
+    }
+
+    return _brightness;
+}
+
+double change_pix_gamma_to_print(unsigned char pix_data, double gamma) {
+    double _brightness = pix_data / 255.0;
+    return change_pix_gamma_to_print(_brightness, gamma);
+}
+
+double change_pix_gamma_from_file(double _brightness, double gamma) {
+    if (gamma > 0) {
+        _brightness = std::pow(_brightness, gamma);
+    } else {
+        _brightness = to_sRGB(_brightness);
+    }
+
+    return _brightness;
+}
+
+double change_pix_gamma_from_file(unsigned char pix_data, double gamma) {
+    double _brightness = pix_data / 255.0;
+    return change_pix_gamma_from_file(_brightness, gamma);
+}
+
+void draw_pix(unsigned char *pix_data, int width, int x, int y, int brightness, double gamma) {
+    double _brightness = brightness / 255.0;
+
+    // Гамма коррекция:
+    change_pix_gamma_to_print(_brightness, gamma);
+
+    pix_data[width * y + x] = limit_brightness(255 * _brightness);
+}
+
+void draw_pix(unsigned char *pix_data, int width, int x, int y, double _brightness, double gamma) {
+    change_pix_gamma_to_print(_brightness, gamma);
+    pix_data[width * y + x] = limit_brightness(255 * _brightness);
+}
+
+void decode_gamma_from_file(unsigned char *pix_data, int k, double gamma) {
+    for (int i = 0; i < k; ++i) {
+        *(pix_data + i) = limit_brightness(255 * change_pix_gamma_from_file(*(pix_data + i), gamma));
+    }
+}
---
+++ Lab_04/write_data_pnm.h
@@ -0,0 +1,36 @@
+//
+// Created by dudko on 11.03.2020.
+//
+
+#ifndef LAB_03_WRITE_DATA_PNM_H
+#define LAB_03_WRITE_DATA_PNM_H
+
+#include <iostream>
+
+void write_header(FILE *file_out, char char_header, int width, int height, unsigned int max_value);
+void write_data(FILE *file_out, int k_bytes, unsigned char *pix_data);
+void free_data(FILE *file_in, FILE *file_out, unsigned char *pix_data);
+void free_data(FILE *file, unsigned char *pix_data);
+void free_data(FILE *file);
+void free_data(unsigned char *pix_data);
+void write_to_file(FILE *file_out, char char_header, int width, int height, unsigned int max_value, unsigned char *pix_data);
+
+unsigned char limit_brightness(double brightness);
+
+double to_sRGB(double _brightness);
+double to_sRGB(int brightness);
+double from_sRGB(double _brightness);
+double from_sRGB(int brightness);
+
+double change_pix_gamma_to_print(double _brightness, double gamma);
+double change_pix_gamma_to_print(unsigned char pix_data, double gamma);
+
+double change_pix_gamma_from_file(double _brightness, double gamma);
+double change_pix_gamma_from_file(unsigned char pix_data, double gamma);
+
+void draw_pix(unsigned char *pix_data, int width, int x, int y, int brightness, double gamma);
+void draw_pix(unsigned char *pix_data, int width, int x, int y, double _brightness, double gamma);
+
+void decode_gamma_from_file(unsigned char *pix_data, int k, double gamma);
+
+#endif //LAB_03_WRITE_DATA_PNM_H
