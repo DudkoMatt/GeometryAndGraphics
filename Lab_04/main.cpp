2dd86d4d8aaba094342a1604c0c021da7ef0db3a
DudkoMatt <github.dudko.matt@mail.ru>
2020-04-28 01:26:58 +0300

    Lab_04: added hsl_2_rgb, removed hsv_2_hsl

--- Lab_04/main.cpp
+++ Lab_04/main.cpp
@@ -178,46 +178,60 @@ void hsv_2_rgb(unsigned char *pix_data, int all_bytes) {
     }
 }
 
-// ToDO:
+// Working
 void hsl_2_rgb(unsigned char *pix_data, int all_bytes) {
-    std::cout << "ToDO";
-}
-
-/*void hsv_2_hsl(unsigned char *pix_data, int all_bytes) {
     for (int i = 0; i < all_bytes; i += 3) {
-        double S = pix_data[i + 1] / 255.0;
-        double V = pix_data[i + 2] / 255.0;
-
-        // H - is unmodified
-        double L = V * (1 - S / 2);
-        double S_l = L == 0 || L == 1 ? 0: (V - L) / std::min(L, 1 - L);
-
-        // To HSL:
-        // H - is unmodified
-        // S:
-        *(pix_data + i + 1) = (unsigned char) S_l * 255;
-        // L:
-        *(pix_data + i + 2) = (unsigned char) L * 255;
-    }
-}
-
-void hsl_2_hsv(unsigned char *pix_data, int all_bytes) {
-    for (int i = 0; i < all_bytes; i += 3) {
-        double S = pix_data[i + 1] / 255.0;
+        double H = pix_data[i] / 255.0 * 360;
+        double S_l = pix_data[i + 1] / 255.0;
         double L = pix_data[i + 2] / 255.0;
 
-        // H - is unmodified
-        double V = L + S * std::min(L, 1 - L);
-        double S_v = V == 0 ? 0 : 2 * (1 - L / V);
+        double C = (1 - std::abs(2 * L - 1)) * S_l;
+        double _H = H / 60;
+        double X = C * (1 - std::abs(((int) _H) % 2 - 1));
 
-        // To HSV:
-        // H - is unmodified
-        // S:
-        *(pix_data + i + 1) = (unsigned char) S_v * 255;
-        // V:
-        *(pix_data + i + 2) = (unsigned char) L * 255;
+        double _R, _G, _B;
+
+        if (std::ceil(_H) == 1) {
+            _R = C;
+            _G = X;
+            _B = 0;
+        } else if (std::ceil(_H) == 2) {
+            _R = X;
+            _G = C;
+            _B = 0;
+        } else if (std::ceil(_H) == 3) {
+            _R = 0;
+            _G = C;
+            _B = X;
+        } else if (std::ceil(_H) == 4) {
+            _R = 0;
+            _G = X;
+            _B = C;
+        } else if (std::ceil(_H) == 5) {
+            _R = X;
+            _G = 0;
+            _B = C;
+        } else if (std::ceil(_H) == 6) {
+            _R = C;
+            _G = 0;
+            _B = X;
+        } else {
+            _R = 0;
+            _G = 0;
+            _B = 0;
+        }
+
+        double m = L - C / 2.0;
+
+        // To RGB:
+        // R:
+        *(pix_data + i)     = (unsigned char) limit_brightness(std::round((_R + m) * 255));
+        // G
+        *(pix_data + i + 1) = (unsigned char) limit_brightness(std::round((_G + m) * 255));
+        // B
+        *(pix_data + i + 2) = (unsigned char) limit_brightness(std::round((_B + m) * 255));
     }
-}*/
+}
 
 // ToDO:
 void rgb_2_YCbCr_601(unsigned char *pix_data, int all_bytes) {
